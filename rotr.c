2626c363cfd83f9e1c4ca1c45aa0caea5b03171c
= <=>
2023-08-25 00:42:13 +0100

    implemented rotr opcode

---
+++ rotr.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates the stack to the bottom, making the
+ * last element the first and leaving the rest intact
+ *
+ * @stack: stack
+ * @line_number: line number of file to execute
+ *
+ * Return: void
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp, *current;
+	(void) *stack;
+	(void) line_number;
+
+	temp = file_ptr->head;
+	if (temp == NULL)
+	{
+		free_file_ptr();
+		return;
+	}
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+
+	temp->next = file_ptr->head;
+	current = temp->prev;
+	temp->prev = NULL;
+	current->next = NULL;
+	file_ptr->head->prev = temp;
+	file_ptr->head = temp;
+}
