2fd86edaec23244c77c26f15a0e4bf8644354f97
khairatAA <khairatadesina01@gmail.com>
2023-08-25 11:15:52 +0100

    dealt with some memory leaks in pop.c..

--- pop.c
+++ pop.c
@@ -15,7 +15,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	temp = file_ptr->head;
 	if (temp == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		fclose_file();
 		free_tokens();
 		free_file_ptr();
